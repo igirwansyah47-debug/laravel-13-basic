a0b13a71bffae88629c22d313e5cb5cb8ff5652a
irwansyah <igirwansyah47@gmail.com>
2026-04-07 12:11:05 +0800

    Pertemuan 3: Database

---
+++ app/Models/Student.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+
+#[Fillable(['name'])]
+
+class Student extends Model
+{
+    /** @use HasFactory<\Database\Factories\StudentFactory> */
+    use HasFactory;
+
+    //     // yang boleh di input
+    // protected $fillable = ['name','nim',];
+
+    // // yang tidak boleh di input
+    // protected $guarded = ['id'];
+}
